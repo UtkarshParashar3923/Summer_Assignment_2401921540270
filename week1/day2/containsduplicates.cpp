9b87fb96ad92c41a63d14a0ddce899d30c7eb0ea
UtkarshParashar3923 <spvivekup@gmail.com>
2026-06-02 08:57:29 +0530

    Update containsduplicates.cpp

--- week1/day2/containsduplicates.cpp
+++ week1/day2/containsduplicates.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    bool hasDuplicates(int arr[5]) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = i + 1; j < 5; j++) {
-                if (arr[i] == arr[j]) {
-                    cout << "true" << endl;
-                    return true;
-                }
+    bool containsDuplicate(vector<int>& nums) {
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] == nums[i-1]) {
+                return true;
             }
         }
+
         return false;
     }
 };
