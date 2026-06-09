6524930d0a4c6f2181f4a689018006b5a68f9c95
Utkarsh Parashar <spvivekup@gmail.com>
2026-06-09 08:23:33 +0530

    Update findallanagram.cpp

--- week2/day2/findallanagram.cpp
+++ week2/day2/findallanagram.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
 
+        vector<int> ans;
+
+        int n = s.size();
+        int m = p.size();
+
+        if (m > n) return ans;
+
+        vector<int> freqP(26, 0);
+        vector<int> freqWin(26, 0);
+
+        for (int i = 0; i < m; i++) {
+            freqP[p[i] - 'a']++;
+            freqWin[s[i] - 'a']++;
+        }
+
+        if (freqP == freqWin)
+            ans.push_back(0);
+
+        for (int i = m; i < n; i++) {
+
+            freqWin[s[i] - 'a']++;
+
+            freqWin[s[i - m] - 'a']--;
+
+            if (freqP == freqWin)
+                ans.push_back(i - m + 1);
+        }
+
+        return ans;
+    }
+};
