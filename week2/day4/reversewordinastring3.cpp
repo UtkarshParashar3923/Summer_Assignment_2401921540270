b30843f5807863c038e92f39500d25280b7eccb3
Utkarsh Parashar <spvivekup@gmail.com>
2026-06-11 08:47:04 +0530

    Update reversewordinastring3.cpp

--- week2/day4/reversewordinastring3.cpp
+++ week2/day4/reversewordinastring3.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    
+    string reverseWord(string word) {
+        if(word.length() <= 1)
+            return word;
 
+        return reverseWord(word.substr(1)) + word[0];
+    }
+
+    string reverseWords(string s) {
+
+        int pos = s.find(' ');
+
+        // Base case: last word
+        if(pos == string::npos)
+            return reverseWord(s);
+
+        string firstWord = s.substr(0, pos);
+
+        return reverseWord(firstWord) + " " +
+               reverseWords(s.substr(pos + 1));
+    }
+};
