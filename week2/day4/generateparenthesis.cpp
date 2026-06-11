8f2387af1ca2a2c86f435ab092408e3b5e66f6fe
Utkarsh Parashar <spvivekup@gmail.com>
2026-06-11 08:58:29 +0530

    Update generateparenthesis.cpp

--- week2/day4/generateparenthesis.cpp
+++ week2/day4/generateparenthesis.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    void backtrack(int open, int close, int maxPairs, string& current, vector<string>& result) {
+        if (current.size() == maxPairs * 2) {
+            result.push_back(current);
+            return;
+        }
+
+        if (open < maxPairs) {
+            current.push_back('(');
+            backtrack(open + 1, close, maxPairs, current, result);
+            current.pop_back();
+        }
+        if (close < open) {
+            current.push_back(')');
+            backtrack(open, close + 1, maxPairs, current, result);
+            current.pop_back();
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        string current;
+        backtrack(0, 0, n, current, result);
+        return result;
+    }
+};
 
