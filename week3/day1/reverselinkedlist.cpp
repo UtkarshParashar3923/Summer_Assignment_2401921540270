266ebda1df230455769960062479fcba9cab819f
Utkarsh Parashar <spvivekup@gmail.com>
2026-06-15 08:27:42 +0530

    Implement reverse linked list function

--- week3/day1/reverselinkedlist.cpp
+++ week3/day1/reverselinkedlist.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
 
+        while(curr != NULL) {
+            next = curr->next;   // store next node
+            curr->next = prev;   // reverse link
+            prev = curr;         // move prev forward
+            curr = next;         // move curr forward
+        }
+
+        return prev;
+    }
+};
