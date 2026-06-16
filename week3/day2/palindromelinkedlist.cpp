ed63a785f1380309d972d7493d42517c71651d92
Utkarsh Parashar <spvivekup@gmail.com>
2026-06-16 08:42:51 +0530

    Update palindromelinkedlist.cpp

--- week3/day2/palindromelinkedlist.cpp
+++ week3/day2/palindromelinkedlist.cpp
@@ -1,1 +1,44 @@
+class Solution {
+public:
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
 
+        while(curr != NULL){
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+            return true;
+
+      
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+       
+        slow = reverse(slow);
+
+     
+        ListNode* temp = head;
+        while(slow != NULL){
+            if(temp->val != slow->val)
+                return false;
+
+            temp = temp->next;
+            slow = slow->next;
+        }
+
+        return true;
+    }
+};
