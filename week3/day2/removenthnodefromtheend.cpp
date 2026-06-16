1f22a23b4ff404613e823cb4e34b9e258edc18be
Utkarsh Parashar <spvivekup@gmail.com>
2026-06-16 08:40:49 +0530

    Update removenthnodefromtheend.cpp

--- week3/day2/removenthnodefromtheend.cpp
+++ week3/day2/removenthnodefromtheend.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // Create a dummy node pointing to head (to handle edge cases)
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
 
+        // First and second pointers
+        ListNode* first = dummy;
+        ListNode* second = dummy;
+
+        for (int i = 0; i <= n; ++i) {
+            first = first->next;
+        }
+
+        while (first != nullptr) {
+            first = first->next;
+            second = second->next;
+        }
+
+        
+        ListNode* nodeToDelete = second->next;
+        second->next = second->next->next;
+
+        
+        delete nodeToDelete;
+
+       
+        ListNode* result = dummy->next;
+        delete dummy;
+        return result;
+    }
+};
