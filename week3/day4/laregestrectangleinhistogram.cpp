5d486537aff6151369e955b20aa225c97c90d26e
Utkarsh Parashar <spvivekup@gmail.com>
2026-06-18 08:42:30 +0530

    Create laregestrectangleinhistogram.cpp

---
+++ week3/day4/laregestrectangleinhistogram.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int ans = 0;
+        stack<int> st;
+        int n = heights.size();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!st.empty() && heights[st.top()] > heights[i]) {
+                int index = st.top();
+                st.pop();
+
+                if (!st.empty())
+                    ans = max(ans, heights[index] * (i - st.top() - 1));
+                else
+                    ans = max(ans, heights[index] * i);
+            }
+
+            st.push(i);
+        }
+
+        
+        while (!st.empty()) {
+            int index = st.top();
+            st.pop();
+
+            if (!st.empty())
+                ans = max(ans, heights[index] * (n - st.top() - 1));
+            else
+                ans = max(ans, heights[index] * n);
+        }
+
+        return ans;
+    }
+};
